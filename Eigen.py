21af90c28290751848f9e4a92ddfc0880000a211
Teja-m9 <ramtejaramteja9322@gmail.com>
2025-03-13 22:52:06 +0530

    add the new files

---
+++ Eigen.py
@@ -0,0 +1,1 @@
+# Research on the new machine learning algorithm for anamoly detection based on the decompostion of covarince matrix where my new algortihm can outperform the Principal component analysis  for anamoly detection.
