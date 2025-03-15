cbdef0bf567cf7618c2695bfcb828571af18fd57
Teja-m9 <ramtejaramteja9322@gmail.com>
2025-03-15 22:55:00 +0530

    add the new files

--- Eigen.py
+++ Eigen.py
@@ -1,2 +1,3 @@
 # Research on the new machine learning algorithm for anamoly detection based on the decompostion of covarince matrix where my new algortihm can outperform the Principal component analysis  for anamoly detection.
 # Eige flux workflow 
+# Eigen flux works on the principle of covariance matrix decomposition and reconstruction of the original data.
